e3f90d77ffb87e08a580ff1ac8ad65be75882385
ShubhamSKadam <shubhamskadam99@outlook.com>
2024-07-13 11:01:42 +0530

    Aloyna Flowers Code

---
+++ Arrays and Dynamic Arrays/C++ Code/15-AlyonaFlowers.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+
+   int n;
+   cin>>n;
+
+   int m;
+   cin>>m;
+
+    vector<int> v(n+1,0);
+    vector<int> prefixSum(n+1,0);
+
+   for(int i=1;i<=n;i++){
+        cin>>v[i]; 
+        // constructing your prefix aray
+        prefixSum[i] = prefixSum[i-1]+ v[i];
+   }
+
+    int happiness = 0;
+    while(m--){
+
+        int l,r;
+        cin>>l>>r; 
+    
+       int subArraySum = prefixSum[r] - prefixSum[l-1]; 
+       if(subArraySum > 0){
+           happiness+=subArraySum;
+       }
+    }
+
+    cout<<happiness<<endl;
+}
