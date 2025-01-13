60a2e6c1e390c8ee3f74582aac6d2ff437d511e8
ShubhamSKadam <shubhamskadam99@outlook.com>
2025-01-13 08:33:12 +0530

    reverseLookup in 2d brutforce update

--- Arrays and Dynamic Arrays/C++ Code/23-reverseLookUp2d.cpp
+++ Arrays and Dynamic Arrays/C++ Code/23-reverseLookUp2d.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
 int getSum(int i, int j, int i1, int j1, vector<vector<int> > &v)
 {
     int sum = 0;
@@ -10,25 +12,23 @@ int getSum(int i, int j, int i1, int j1, vector<vector<int> > &v)
             sum += v[k][l];
         }
     }
-    cout << "Sum for " << i << " iteration" << "is" << sum << endl;
+    cout << "Sum for submatrix (" << i << ", " << j << ") to (" << i1 << ", " << j1 << ") is " << sum << endl;
     return sum;
 }
+
 int main()
 {
-
     int n, m;
     cin >> n >> m;
 
-    // * 2D vector declaration
-    // vector<vector<int>> v;
-
-    // * Initialization using the length
+    // Initialize 2D vector
     vector<vector<int> > v(n, vector<int>(m));
 
+    // Input matrix values
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
-        {
+        for (int j = 0; j < m; j++)
+        { // Fix: Iterate up to m, not n
             cin >> v[i][j];
         }
     }
@@ -36,17 +36,18 @@ int main()
     int ans = 0;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
-        {
+        for (int j = 0; j < m; j++)
+        { // Fix: Iterate up to m, not n
             for (int i1 = i; i1 < n; i1++)
-            {
-                for (int j1 = j; j1 < n; j1++)
-                {
+            { // Fix: Use i1 < n
+                for (int j1 = j; j1 < m; j1++)
+                { // Fix: Use j1 < m
                     ans += getSum(i, j, i1, j1, v);
                 }
             }
         }
     }
 
-    cout << ans << endl;
-};
+    cout << "Total sum of all submatrices: " << ans << endl;
+    return 0;
+}
--- Arrays and Dynamic Arrays/JS Code/01-precomputation.js
+++ Arrays and Dynamic Arrays/JS Code/01-precomputation.js
@@ -1,22 +1,20 @@
 const inputArr = [1, 2, 3, 4, 5, 6];
 
 for (let i = 1; i < inputArr.length; i++) {
-  inputArr[i] += inputArr[i - 1]
+  inputArr[i] += inputArr[i - 1];
 }
 
 // let's say I want to find the sum from a starting index 'l' and ending index
-// 'r', then we can find it using inputArr[r]-inputArr[l-1]. 
+// 'r', then we can find it using inputArr[r]-inputArr[l-1].
 
 let l = 1,
   r = 4;
 console.log(inputArr[r] - inputArr[l - 1]); // 14
 
-// You have precomputed the array and now you can do any number of 
+// You have precomputed the array and now you can do any number of
 // queries in just O(1) time.
 
 // Total TC : O(n)
 
-// Remember we can also do a suffix sum by just iterating the array 
+// Remember we can also do a suffix sum by just iterating the array
 // backwards
-
-
